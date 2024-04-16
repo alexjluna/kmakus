987493fd8d585d6d5a0d5c029fc6050e5963a554
Alex J. Luna <alejolumend@gmail.com>
2024-04-16 13:34:09 +0200

    kmakus theme

---
+++ web/themes/custom/kmakus/js/kmakus.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * kmakus behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.kmakus = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
